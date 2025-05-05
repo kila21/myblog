10e9a593056c46a88f4049744b3b2b3a4fee714e
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-05 14:11:22 +0400

    [users] create custom user model

--- backend/backend/users/models.py
+++ backend/backend/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    full_name = models.CharField(max_length=150, blank=True)
+
+    def __str__(self):
+        return self.username

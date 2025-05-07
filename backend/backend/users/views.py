ae971e3a2ec34a407808fe2eee3afd16d3fc2f9b
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-07 14:37:05 +0400

    [users.views] create register

--- backend/backend/users/views.py
+++ backend/backend/users/views.py
@@ -2,16 +2,24 @@ from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-from users import serializers as users_serializer
+from users import serializers as users_serializers
 from users import models as users_models
 
 # Create your views here.
 
+## returns profile of user. which also contains user object.
 class ProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = users_serializer.ProfileSerializer
+    serializer_class = users_serializers.ProfileSerializer
     permission_classes = [AllowAny]
 
     def get_object(self):
         user_id = self.kwargs['user_id']
         profile = users_models.Profile.objects.get(user__id=user_id)
         return profile
+    
+## Register View
+
+class RegisterView(generics.CreateAPIView):
+    queryset = users_models.User.objects.all()
+    serializer_class = users_serializers.RegisterSerializer
+    permission_classes = [AllowAny]

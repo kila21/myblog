08d90b173f55849050d89cfb26517c54c25fc3e8
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-07 14:36:44 +0400

    [users.serializer] create register serializer

--- backend/backend/users/serializers.py
+++ backend/backend/users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 
 from .models import User, Profile
 
@@ -17,3 +18,29 @@ class ProfileSerializer(serializers.ModelSerializer):
         response = super().to_representation(instance)
         response['user'] = UserSerializer(instance.user).data
         return response
+    
+## Register Serializer
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'}, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'full_name', 'password', 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError('Passwords do not match!')
+        return attrs
+    
+    def create(self, validated_data):
+        user = User.objects.create(
+            email=validated_data['email'],
+            username=validated_data['username'],
+            full_name=validated_data['full_name']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+    
+        return user

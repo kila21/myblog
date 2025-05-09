1abf42266b845b0f55cdedbd599d1ae440867f4f
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-09 16:44:42 +0400

    [errors] add comma in tuple

--- backend/backend/backend/settings.py
+++ backend/backend/backend/settings.py
@@ -133,6 +133,6 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication'
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }

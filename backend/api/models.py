c3282c45fc27efc882ddd8f505eb223b91e1091f
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-13 18:34:17 +0400

    [api.models] create post model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from django.utils.text import slugify
 
+from django.conf import settings
+
+from users import models as users_models
+
 ## category and post
 
 
@@ -20,3 +24,26 @@ class Category(models.Model):
             self.slug = slugify(self.title)
         return super().save(*args, **kwargs)
     
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    profile = models.ForeignKey(users_models.Profile, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=100)
+    image = models.FileField(upload_to="image", null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    tags = models.CharField(max_length=100)
+    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    view = models.IntegerField(default=0)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    slug = models.SlugField(unique=True, blank=True, null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    
+    class Meta:
+        verbose_name_plural = 'Post'

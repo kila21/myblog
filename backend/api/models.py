b59706c3dbccdfc366cf71dad09192ded8e0f7a6
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-13 18:56:58 +0400

    [users.models] update post model fields and save method

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils.text import slugify
+import shortuuid
 
 from django.conf import settings
 
@@ -26,14 +27,13 @@ class Category(models.Model):
     
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    profile = models.ForeignKey(users_models.Profile, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=100)
-    image = models.FileField(upload_to="image", null=True, blank=True)
+    image = models.ImageField(upload_to="image", null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     tags = models.CharField(max_length=100)
-    Category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     view = models.IntegerField(default=0)
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='liked_posts')
     slug = models.SlugField(unique=True, blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
 
@@ -42,8 +42,12 @@ class Post(models.Model):
     
     def save(self, *args, **kwargs):
         if not self.slug:
-            self.slug = slugify(self.title)
-        return super().save(*args, **kwargs)
+            base_slug = slugify(self.title)
+            unique = base_slug + "-" + shortuuid.uuid()[:2]
+            while Post.objects.filter(slug=unique).exists():
+                unique = base_slug + "-" + shortuuid.uuid()[:2]
+            self.slug = unique
+        super().save(*args, **kwargs)
     
     class Meta:
         verbose_name_plural = 'Post'

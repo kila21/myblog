0c6b5df615a8341dd7969affd10515dee52d8a37
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-13 18:10:58 +0400

    [api.models] create Category model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+## category and post
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='category', blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True)
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name_plural = 'Category'
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
+    

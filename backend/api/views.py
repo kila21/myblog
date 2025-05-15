60c6e3a1f7007fa20eb69df4ae005b819776d165
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-15 21:26:15 +0400

    [api.views] create post list view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+
+from api import models as api_models
+from api import serializers as api_serializers
 
 # Create your views here.
+
+class PostListAPIView(generics.ListAPIView):
+    serializer_class = api_serializers.PostSerializer
+    permission_classes = [AllowAny]
+
+    def get_queryset(self):
+        return api_models.Post.objects.all()

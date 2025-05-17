5e71bbc573bf8ff74fd2068fea5afd09a8cf588a
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-17 16:12:01 +0400

    [api.urls] post create url

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,5 +6,6 @@ from api import views as api_vivews
 urlpatterns = [
      #token
      path('posts/', api_vivews.PostListAPIView.as_view()),
+     path('posts/create/', api_vivews.PostCreateApiView.as_view())
 
 ]

ec41166a230cd7b71ededaf079bd398bf1f29610
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-30 10:34:30 +0400

    [api.views] add post detail and update view

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework import generics
 
 from api import models as api_models
 from api import serializers as api_serializers
 
-# Create your views here.
 
+# Post Serializers
 class PostListAPIView(generics.ListAPIView):
     serializer_class = api_serializers.PostSerializer
     permission_classes = [AllowAny]
@@ -15,9 +15,19 @@ class PostListAPIView(generics.ListAPIView):
         return api_models.Post.objects.all()
     
 
-class PostCreateApiView(generics.CreateAPIView):
+class PostCreateAPIView(generics.CreateAPIView):
     serializer_class = api_serializers.PostCreateSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
         return serializer.save(author=self.request.user)
+    
+class PostDetailAPIView(generics.RetrieveUpdateAPIView):
+    serializer_class = api_serializers.PostSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        post = api_models.Post.objects.get(slug=slug)
+        return post
+

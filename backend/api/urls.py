ec0e623ec5aed7cfb8ed84d83f14781f00174ebd
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-30 20:29:25 +0400

    [update] update urls create and detail

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,9 +4,8 @@ from django.urls import path
 from api import views as api_views
 
 urlpatterns = [
-     #token
+     ## posts
      path('posts/', api_views.PostListAPIView.as_view()),
-     path('posts-create/', api_views.PostCreateAPIView.as_view()),
-     path('posts/<slug:slug>/', api_views.PostDetailAPIView.as_view())
-
+     path('posts/create/', api_views.PostCreateAPIView.as_view()),
+     path('posts/detail/<slug:slug>/', api_views.PostDetailAPIView.as_view()),
 ]

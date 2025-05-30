3759308c0bb260f312cd3e7f19cc4bffb20519a7
kila <luka.kilasonia.1@btu.edu.ge>
2025-05-30 11:58:54 +0400

    [api.urls] change order and name of post create

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,7 +6,7 @@ from api import views as api_views
 urlpatterns = [
      #token
      path('posts/', api_views.PostListAPIView.as_view()),
-     path('posts/<slug:slug>/', api_views.PostDetailAPIView.as_view()),
-     path('posts/create/', api_views.PostCreateAPIView.as_view())
+     path('posts-create/', api_views.PostCreateAPIView.as_view()),
+     path('posts/<slug:slug>/', api_views.PostDetailAPIView.as_view())
 
 ]

1d246db95611fe02ad9728b1f4713a5bcb06c6ea
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-02 00:03:01 +0400

    [page.home] create main section

---
+++ frontend/src/components/page/Home.tsx
@@ -0,0 +1,12 @@
+
+export const Home = () => {
+    return (
+        <main id="main-content" className="flex justify-center text-center">
+            <section aria-labelledby='Title' className="max-w-115 h-auto flex flex-col justify-between space-y-5 items-center">
+                <h1>Blog App</h1>
+                <hr className="gradiant-main h-0.5 w-25 border-none"/>
+                <p className="px-3">Norrsken is a simple block theme for blogs, with a stylish design inspired by the aurora borealis.</p>
+            </section>
+        </main>
+    )
+}

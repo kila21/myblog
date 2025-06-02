1ecd4f936f2080794b46b37e8decf6890a1eb3d1
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-02 20:28:04 +0400

    [layout] create footer component

---
+++ frontend/src/components/layout/partials/Footer.tsx
@@ -0,0 +1,14 @@
+export const Footer = () => {
+
+    return (
+        <footer className='flex flex-col items-center w-full px-3 gap-5'>
+            <hr className="gradiant-main h-0.5 w-50 border-none mb-7"/>
+            <h3 className="text-white font-bold"></h3>
+            <p className="text-lightgrey">Theme by <a target="_blank" href="https://andersnoren.se">Anders Norén</a>
+            </p>
+            <p className="text-lightgrey mb-10">
+                Site Built By<em> Luka Kilasonia</em>
+            </p>
+        </footer>
+    )
+}

3884ed544be0575d03e90f4cde60d20cac12192c
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-03 07:09:39 +0400

    [update] change div to main

--- frontend/src/page/auth/Login.tsx
+++ frontend/src/page/auth/Login.tsx
@@ -4,9 +4,9 @@ import { LoginForm } from "../../components/forms/LoginForm"
 
 export const Login = () => {
     return (
-        <div className="w-full h-full flex flex-col justify-center items-center">
+        <main className="w-full h-full flex flex-col justify-center items-center">
             <h1 className="text-center text-2xl font-bold text-gray-600 mb-6">LOGIN</h1>
             <LoginForm />
-        </div>
+        </main>
     )
 }

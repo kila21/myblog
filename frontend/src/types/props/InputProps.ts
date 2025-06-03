89e600a47e888eadc03545d8b2501615c820c907
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-03 04:19:04 +0400

    [types] create Custom Input props type

---
+++ frontend/src/types/props/InputProps.ts
@@ -0,0 +1,9 @@
+import { type InputHTMLAttributes } from 'react'
+// import type { FieldError } from "react-hook-form";
+
+
+export type InputProps = {
+    label: string;
+    // error?: FieldError;
+    error?: string;
+} & InputHTMLAttributes<HTMLInputElement>;

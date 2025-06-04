7b35c062128021dee4fcc435e6ba911c6ff2e98f
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-04 20:14:58 +0400

    [services] get Single Post data

--- frontend/src/services/commonService.ts
+++ frontend/src/services/commonService.ts
@@ -1,9 +1,17 @@
 import type { AxiosResponse } from "axios"
 
 import api from "./axios"
-import type { PostResponseType } from "../types/post/postResponse"
+import type { PostResponseType } from "../types/post/PostResponse"
 
-// for home pages. now get all post, later shoud be top.
+// for home pages. now get all post, later shoud be top(like filter with tags.).
 export const getTopPosts = (): Promise<AxiosResponse<PostResponseType[]>> => {
     return api.get('/api/posts/')
+}
+
+export const getAllPost = (): Promise<AxiosResponse<PostResponseType[]>> => {
+    return api.get('/api/posts/')
+}
+
+export const getDetailPost = (slug: string): Promise<AxiosResponse<PostResponseType>> => {
+    return api.get(`/api/posts/detail/${slug}/`)
 }

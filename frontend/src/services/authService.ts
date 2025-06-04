da66aa9d50a8398ce538bce0a5b7de80458072d9
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-04 08:10:21 +0400

    [services]  create register service

--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -2,8 +2,15 @@ import type { AxiosResponse } from "axios";
 
 import api from "./axios";
 import type { LoginFormDataType } from "../types/auth/LoginFormData";
+import type { RegisterFormDataType } from "../types/auth/RegisterFormData";
+
 import type { LoginResponseType } from "../types/auth/LoginResponse";
+import type { RegisterResponseType } from "../types/auth/RegisterResponse";
 
 export const loginUser = (data: LoginFormDataType): Promise<AxiosResponse<LoginResponseType>> => {
     return api.post('/users/token/', data)
+}
+
+export const registerUser = (data: RegisterFormDataType): Promise<AxiosResponse<RegisterResponseType>> => {
+    return api.post('/users/register/', data)
 }

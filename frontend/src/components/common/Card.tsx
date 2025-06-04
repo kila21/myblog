0f7ae08174ddb1893f53458ef0482817f0a4f669
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-04 08:56:50 +0400

    [card] dispay card info from database

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -1,15 +1,14 @@
+import type { CardPropsType } from "../../types/props/CardPropsType"
 
-
-export const Card = () =>{
+export const Card = (props: CardPropsType) =>{
     return (
         <article className="flex flex-col w-70 sm:w-50">
             {/* <img src="" alt=""/> */}
-            <div className="w-full h-48 bg-white rounded-2xl"></div>
+            <div className="w-full h-48 bg-amber-100 rounded-2xl"></div>
             <div className="w-full p-3 mt-4 flex flex-col gap-2">
-                <h3 className="text-md text-lightgrey">Blog Title</h3> 
-                <time className="text-lightgrey text-sm" dateTime="2025-06-01">June 1, 2025</time>
+                <h3 className="text-md text-lightgrey">{props.title}</h3> 
+                <time className="text-lightgrey text-sm" dateTime="2025-06-01">{props.date}</time>
             </div>
-
         </article>
     )
 }

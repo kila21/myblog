cddda31ca4ad680edcb939427292ba583130fc51
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-04 09:21:49 +0400

    [header] check if user is auth or not

--- frontend/src/components/layout/partials/Header.tsx
+++ frontend/src/components/layout/partials/Header.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Cookies from 'js-cookie';
 
 import {Menu, X } from 'lucide-react';
 
@@ -6,18 +7,37 @@ import {Menu, X } from 'lucide-react';
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
 
+    const [isAuth, setIsAuth ] = useState(false)
+
+    const getAuthToken = () => {
+        const token = Cookies.get('access_token')
+        if (token) {
+            setIsAuth(true)
+        }
+    }
+
+    useEffect(() => {
+        getAuthToken()
+    },)
+
     return (
         <header className='flex justify-between items-center w-full h-15 fixed top-0 left-0 px-3'>
             <nav className='hidden w-auto h-6 md:flex items-center justify-around space-x-5 ml-2'>
-                <a href='#'><li>Home</li></a>
+                <a href='/'><li>Home</li></a>
                 <a href='#'><li>Posts</li></a>
                 <a href='#'><li>Search</li></a>
             </nav>
 
             {/*user profile icon */}
-            <div className='w-10 h-10 rounded-full overflow-hidden border border-grey-300 shadow-sm mr-5'>
-                <img className='w-full h-full object-cover bg-white' src='' alt=''/>
-            </div>
+            {isAuth ? 
+                <div className='w-10 h-10 rounded-full overflow-hidden border border-grey-300 shadow-sm mr-5'>
+                    <img className='w-full h-full object-cover bg-white' src='' alt=''/>
+                </div>
+                :
+                <div className='w-30 h-10 mt-5'>
+                    <a href='/login'>Login</a>
+                </div>
+            }
             
             {/* mobile navigation */}
             {isOpen ? 

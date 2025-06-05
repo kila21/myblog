5bf4a2f618f193ac27e9a0a491ffb7c74081e316
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-05 15:28:51 +0400

    [update] profile posts styles

--- frontend/src/page/profile/ProfilePosts.tsx
+++ frontend/src/page/profile/ProfilePosts.tsx
@@ -3,17 +3,13 @@ import { Card } from "../../components/common/Card"
 import { getUserPosts } from "../../services/commonService";
 import type { PostResponseType } from "../../types/post/PostResponse";
 
-export type ProfilePostProps = {
-    user_id: number;
-    username: string,
-}
 
-export const ProfilePosts = (props: ProfilePostProps) => {
+export const ProfilePosts = (props: {username: string}) => {
     const [posts, setPosts ] = useState<PostResponseType[]>()
 
     const fetchUserPosts = async () => {
         try {
-            const response = await getUserPosts(props.user_id)
+            const response = await getUserPosts(props.username)
             if (response.data) {
                 setPosts(response.data)
             }
@@ -27,9 +23,9 @@ export const ProfilePosts = (props: ProfilePostProps) => {
     }, [])
 
     return (
-        <div className="w-full flex flex-col gap-3">
-            <h3 className="bold text-white mb-5">{props.username}'s posts:</h3>
-            <div className="flex flex-wrap gap-10">
+        <div className="w-full flex flex-col items-center gap-3 mb-10">
+            <h3 className="bold text-white mb-5 uppercase">{props.username}'s posts:</h3>
+            <div className="flex justify-around flex-wrap gap-10">
                 {posts && posts.map((post: PostResponseType) => {
                     return <Card 
                     img={''} 

c64098e4aaaa2f489b2cb0abea780773f3882a8e
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-05 10:17:18 +0400

    [api.urls] update url of userPostsList

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
      path('posts/', api_views.PostListAPIView.as_view()),
      path('posts/create/', api_views.PostCreateAPIView.as_view()),
      path('posts/detail/<slug:slug>/', api_views.PostDetailAPIView.as_view()),
-     path('posts/user-posts/', api_views.UserPostsListView.as_view(), name='user-posts'),
+     path('posts/user-posts/<int:user_id>/', api_views.UserPostsListView.as_view(), name='user-posts'),
      path('posts/delete/<slug:slug>/', api_views.PostDeleteApiView.as_view(), name='post-delete'),
 
      ## comments

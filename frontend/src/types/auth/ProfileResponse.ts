f9538006d80debab749ed4b2cbc66c1996125edc
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-05 09:13:47 +0400

    [types] profile response type

---
+++ frontend/src/types/auth/ProfileResponse.ts
@@ -0,0 +1,17 @@
+
+
+export type ProfileResponseType = {
+    id: number,
+    user: number,
+    username: string,
+    about: string,
+    bio: string,
+    author:boolean,
+    email: string,
+    full_name: string | null,
+    date: string,
+    country: string,
+    image: string | null,
+    twitter: string | null,
+    facebook: string | null,
+}

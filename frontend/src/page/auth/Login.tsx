8961fc63f66a08783b64aaa6101e9dfac68d0fda
kila <luka.kilasonia.1@btu.edu.ge>
2025-06-18 13:26:39 +0400

    navigate to home from auth pages

--- frontend/src/page/auth/Login.tsx
+++ frontend/src/page/auth/Login.tsx
@@ -1,10 +1,15 @@
 
+import { Link } from "react-router-dom"
 import { LoginForm } from "../../components/forms/LoginForm"
 
 
 export const Login = () => {
     return (
-        <main className="w-full h-full flex flex-col justify-center items-center">
+        <main className="w-full h-full flex flex-col justify-center items-center px-10">
+            <Link to={'/'} className="header max-w-110 text-center mb-10 cursor-pointer absolute top-5 left-5 md:top-10 md:left-10">
+                <h1 className="mb-3 ">Blog App</h1>
+                <hr className="gradiant-main h-0.5 w-25 border-none"/>
+            </Link>
             <h1 className="text-center text-2xl font-bold text-gray-600 mb-6">LOGIN</h1>
             <LoginForm />
         </main>

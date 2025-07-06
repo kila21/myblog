ada46fec21afa87411ef9b8ccca321a5800e5137
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-06 05:25:32 +0400

    [update] navigate when click title or image

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -9,9 +9,9 @@ export const Card = (props: CardPropsType) =>{
     const authState = useAppSelector((state) => state.auth)
 
     return (
-        <article className="flex flex-col w-80 sm:w-70" onClick={() => navigate(`/post/${props.slug}`)}>
+        <article className="flex flex-col w-80 sm:w-70">
             {/* <img src="" alt=""/> */}
-            <div className="w-full h-48 bg-amber-100 rounded-2xl"></div>
+            <div className="w-full h-48 bg-amber-100 rounded-2xl" onClick={() => navigate(`/post/${props.slug}`)}></div>
             <div className="w-full pl-3 mt-4 flex flex-col gap-2">
                 <div className="flex items-center justify-between">
                     <div className="flex gap-2">
@@ -34,7 +34,7 @@ export const Card = (props: CardPropsType) =>{
                         <span>{props.views}</span>
                     </div>
                 </div>
-                <h3 className="text-md text-lightgrey">{props.title}</h3> 
+                <h3 className="text-md text-lightgrey cursor-pointer" onClick={() => navigate(`/post/${props.slug}`)}>{props.title}</h3> 
                 <time className="text-lightgrey text-sm" dateTime="2025-06-01">{props.date}</time>
             </div>
         </article>

73027e4d331f249ba546b8347757ca5a575d53bc
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-06 05:23:23 +0400

    [card] user interactions display with color

--- frontend/src/components/common/Card.tsx
+++ frontend/src/components/common/Card.tsx
@@ -1,27 +1,36 @@
 import { useNavigate } from "react-router-dom"
 import type { CardPropsType } from "../../types/props/CardPropsType"
 import { Bookmark, Eye, Heart } from "lucide-react"
+import { useAppSelector } from "../../store/hooks"
+
 
 export const Card = (props: CardPropsType) =>{
     const navigate = useNavigate()
+    const authState = useAppSelector((state) => state.auth)
+
     return (
         <article className="flex flex-col w-80 sm:w-70" onClick={() => navigate(`/post/${props.slug}`)}>
             {/* <img src="" alt=""/> */}
             <div className="w-full h-48 bg-amber-100 rounded-2xl"></div>
             <div className="w-full pl-3 mt-4 flex flex-col gap-2">
-                <div className="flex items-center justify-around">
+                <div className="flex items-center justify-between">
                     <div className="flex gap-2">
-                        <Heart />
+                        <Heart 
+                        className="cursor-pointer"
+                        color={(authState.user && props.is_liked) ? 'red' : 'grey'}
+                        />
                         <span>{props.likes}</span>
                     </div>
 
                     <div className="flex gap-2">
-                        <Bookmark />
+                        <Bookmark 
+                        className="cursor-pointer" 
+                        color={(authState.user && props.is_bookmarked)? 'red' : 'grey'}/>
                         <span>{props.bookmarks}</span>
                     </div>
 
-                    <div className="flex">
-                        <Eye />
+                    <div className="flex gap-2">
+                        <Eye color="grey"/>
                         <span>{props.views}</span>
                     </div>
                 </div>

38cc0485d72b6c4f84b1a5e05aa29bb31b84a9e3
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-15 18:28:18 +0400

    [store] add post service into reducers

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from './auth/authSlice'
+import { postsApi } from "./posts/postsService";
 
 
 export const store = configureStore({
     reducer: {
         'auth': authReducer,
-    }
+        [postsApi.reducerPath]: postsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => 
+        getDefaultMiddleware().concat(postsApi.middleware)
 })
 
 export type RootState = ReturnType<typeof store.getState>

7417ce6ab21796a249781903c1122a2e3f07017f
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-15 15:32:15 +0400

    [skeleton] create single card skeleton

--- frontend/src/components/skeletons/CardSkeleton.tsx
+++ frontend/src/components/skeletons/CardSkeleton.tsx
@@ -2,5 +2,35 @@ import Skeleton from "react-loading-skeleton"
 
 
 export const CardSkeleton = () => {
-    return <Skeleton count={1} width={50} height={100} baseColor="#d1d5db" highlightColor="#e5e7eb"></Skeleton>
+    return (
+        <div className="flex flex-col w-80 sm:w-70 rounded-2xl">
+            <Skeleton height={'10rem'}></Skeleton>
+            <div className="flex gap-2 justify-between">
+                <div className="flex gap-1">
+                    <Skeleton width={25}></Skeleton>
+                    <Skeleton width={10}></Skeleton>
+                </div>
+                
+                <div className="flex gap-1">
+                    <Skeleton width={25}></Skeleton>
+                    <Skeleton width={10}></Skeleton>
+                </div>
+
+                <div className="flex gap-1">
+                    <Skeleton width={25}></Skeleton>
+                    <Skeleton width={10}></Skeleton>
+                </div>
+            </div>
+            
+            <div className="flex flex-col items-center w-full">
+                <div className="w-11/12">
+                    <Skeleton width={'100%'} height={15}></Skeleton>
+                </div>
+                <div className="w-3/5">
+                    <Skeleton width={'100%'} height={15}></Skeleton>
+                </div>
+            </div>
+        </div>
+    )
+
 }

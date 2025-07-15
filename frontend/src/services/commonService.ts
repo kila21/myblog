b8e2c6f2dedab561dd954e8414ca823173e9a15e
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-15 18:54:53 +0400

    [service] remove uneccessary endpoints

--- frontend/src/services/commonService.ts
+++ frontend/src/services/commonService.ts
@@ -8,13 +8,6 @@ export const getTopPosts = (): Promise<AxiosResponse<PostResponseType[]>> => {
     return api.get('/api/posts/')
 }
 
-export const getAllPost = (): Promise<AxiosResponse<PostResponseType[]>> => {
-    return api.get('/api/posts/')
-}
-
-export const getDetailPost = (slug: string): Promise<AxiosResponse<PostResponseType>> => {
-    return api.get(`/api/posts/detail/${slug}/`)
-}
 
 // get user all post as list,by user id
 export const getUserPosts = (username: string): Promise<AxiosResponse<PostResponseType[]>> => {

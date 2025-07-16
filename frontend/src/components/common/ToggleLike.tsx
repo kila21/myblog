554e72a983704740867747f1200b654491a23ec2
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-16 20:56:00 +0400

    [toggleLike] update handleToggleLike

--- frontend/src/components/common/ToggleLike.tsx
+++ frontend/src/components/common/ToggleLike.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react"
 import { Heart } from "lucide-react"
 
-import { togglePostLike } from "../../services/commonService"
 import { useAppSelector } from "../../store/hooks"
+import { useToggleLikeMutation } from "../../store/posts/postsService"
 
 
 export const ToggleLike = (props: {slug: string, liked: boolean, count: number}) => {
+
+    const [toggleLike] = useToggleLikeMutation()
+
     const authState = useAppSelector(state => state.auth)
     const [likeData, setLikeData] = useState<{liked: boolean, likesCount: number}>({liked: props.liked, likesCount: props.count})
 
@@ -13,12 +16,16 @@ export const ToggleLike = (props: {slug: string, liked: boolean, count: number})
     const handleToggleLike = async () => {
         if (authState.user) {
             try {
-                const response = await togglePostLike(props.slug)
-                if (response.status === 200 && likeData.liked) {
-                    setLikeData(prev => ({liked: false, likesCount: prev.likesCount - 1}))
-                } else {
+                // const response = await togglePostLike(props.slug)
+                const response = await toggleLike(props.slug).unwrap()
+
+                if (response.likes.startsWith('Liked')) {
                     setLikeData(prev => ({liked: true, likesCount: prev.likesCount + 1}))
+                } else {
+                    setLikeData(prev => ({liked: false, likesCount: prev.likesCount - 1}))
                 }
+
+                console.log(response)
             } catch(err) {
                 // error message for ui
                 alert('Like wont Work pls try Again.' + err)

5ca8d755d3d88b9f19839088b9e5a5affaf5de4e
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-16 19:29:44 +0400

    [update] postService header and provides tag for refreshing

--- frontend/src/store/posts/postsService.ts
+++ frontend/src/store/posts/postsService.ts
@@ -3,15 +3,27 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { API_BASE_URL } from "../../constants/api";
 import type { PostResponseType } from "../../types/post/PostResponse";
 
+import type { RootState } from '../store'
+
 
 
 export const postsApi = createApi({
     reducerPath: 'postsApi',
-    baseQuery: fetchBaseQuery({baseUrl: `${API_BASE_URL}`}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${API_BASE_URL}`,
+        prepareHeaders: (headers, { getState }) => {
+            const token = (getState() as RootState).auth.token
+            if (token) {
+                headers.set('Authorization', `Bearer ${token}`)
+            }
+            return headers
+        }
+    }),
     tagTypes: ['Post'],
     endpoints: (builder) => ({
         getPosts: builder.query<PostResponseType[], void>({
-            query: () => 'api/posts/'
+            query: () => 'api/posts/',
+            providesTags: ['Post']
         }),
         getPost: builder.query<PostResponseType, string>({
             query: (slug: string) => `api/posts/detail/${slug}`

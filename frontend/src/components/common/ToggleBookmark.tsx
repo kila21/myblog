3ebd51fb2437eda4a440199e119e3ea1ac9dfdb7
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-18 17:38:19 +0400

    [update] remove useStates, dont need anymore

--- frontend/src/components/common/ToggleBookmark.tsx
+++ frontend/src/components/common/ToggleBookmark.tsx
@@ -1,27 +1,16 @@
-import { useState } from "react"
 import { Bookmark } from "lucide-react"
 
 import { useAppSelector } from "../../store/hooks"
-
 import { useTogglePostBookmarkMutation } from "../../store/posts/postsService"
 
 export const ToggleBookmark = (props: {slug: string, bookmarked: boolean, count: number}) => {
     const authState = useAppSelector((state) => state.auth)
-
     const [ togglePostBookmark ] = useTogglePostBookmarkMutation()
-    
-    const [bookmarkData, setBookmarkData] = useState<{bookmarked: boolean, bookmarksCount: number}>({bookmarked: props.bookmarked, bookmarksCount: props.count})
 
     const handleToggleBookmark = async () => {
         if (authState.user) {
             try {
-                const response = await togglePostBookmark(props.slug).unwrap()
-
-                if(response.bookmarked.includes('Removed')) {
-                    setBookmarkData(prev => ({bookmarked: false, bookmarksCount: prev.bookmarksCount - 1}))
-                }else {
-                    setBookmarkData(prev => ({bookmarked: true, bookmarksCount: prev.bookmarksCount + 1}))
-                }
+                await togglePostBookmark(props.slug).unwrap()
             } catch (err) {
                 alert('Toggle Bookmark Wont Work, Please Try Again!.' + err)
             }
@@ -35,10 +24,10 @@ export const ToggleBookmark = (props: {slug: string, bookmarked: boolean, count:
         <div className="flex gap-2">
             <Bookmark 
             className="cursor-pointer" 
-            color={(authState.user && bookmarkData.bookmarked)? 'red' : 'grey'}
+            color={(authState.user && props.bookmarked)? 'red' : 'grey'}
             onClick={handleToggleBookmark}
             />
-            <span>{bookmarkData.bookmarksCount}</span>
+            <span>{props.count}</span>
         </div>
     )
 }

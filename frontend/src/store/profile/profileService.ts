ac09bfd8f24b5173c4f7340e2eb808f5920f49f0
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-19 19:48:40 +0400

    [redux] add functions for user posts and authenticated user

--- frontend/src/store/profile/profileService.ts
+++ frontend/src/store/profile/profileService.ts
@@ -1,19 +1,34 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
 import { API_BASE_URL } from "../../constants/api";
+
 import type { ProfileResponseType } from "../../types/auth/ProfileResponse";
+import type { PostResponseType } from "../../types/post/PostResponse";
 
 export const profileApi = createApi({
     reducerPath: 'profileApi',
     baseQuery: fetchBaseQuery({baseUrl: API_BASE_URL}),
-    tagTypes: ['UserProfile'],
+    tagTypes: ['UserProfile', 'UserPosts', 'AuthenticatedUser'],
     endpoints: (builder) => ({
+        getAuthenticatedUserProfile: builder.query<ProfileResponseType, string>({
+            query: (username: string) => `/users/profile/${username}/`,
+            providesTags: (_result, _err, username) => [{type: 'AuthenticatedUser', id: username}]
+        }),
+
         getUserProfile: builder.query<ProfileResponseType, string>({
             query: (username: string) => `/users/profile/${username}/`,
             providesTags: (_result, _err, username) => [{type: 'UserProfile', id: username}]
         }),
+
+        getUserPosts: builder.query<PostResponseType[], string>({
+            query: (username: string) => `/api/posts/user-posts/${username}/`,
+            providesTags: (_result, _err, username) => [{type: 'UserPosts', id: username}]
+        })
     })
 })
 
 export const {
+    useGetAuthenticatedUserProfileQuery,
     useGetUserProfileQuery,
+    useGetUserPostsQuery,
 } = profileApi

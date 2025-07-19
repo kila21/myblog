5a814d703b5b86f4bbd8b0abfdb34bce1af0c4c2
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-19 19:23:02 +0400

    [update] functionality of profile page data fetching

--- frontend/src/page/profile/Profile.tsx
+++ frontend/src/page/profile/Profile.tsx
@@ -1,80 +1,71 @@
-import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { getUserData } from "../../services/authService"
-import type { ProfileResponseType } from "../../types/auth/ProfileResponse"
+
 import { Facebook, Twitter } from "lucide-react"
 import { formatDate } from "../../utils/date"
 import { ProfilePosts } from "./ProfilePosts"
+import { ProfilePageSkeleton } from "../../components/skeletons/ProfilePageSkeleton"
+import { useGetUserProfileQuery } from "../../store/profile/profileService"
 
 
 export const Profile = () => {
     const params = useParams()
-
-    const [profileData, setProfileData] = useState<ProfileResponseType>()
-
-    const fetchUser = async (username: string) => {
-        try {
-            const response = await getUserData(username)
-            setProfileData(response.data)
-        }catch (err) {
-            console.log(err)
-        }
-    }
-
-    useEffect(() => {
-        fetchUser(params.username!)
-    }, [])
-
+    const { data, isError, isLoading } = useGetUserProfileQuery(params.username!)
 
     return (
-        <div className="px-4 flex flex-col">
-            <main className="w-full flex flex-col md:flex-row md:justify-between">
-                {/* user image, bio  */}
-                <div className="w-full md:w-1/2 flex flex-col items-center gap-2">
-                    <div className='w-50 h-50 rounded-full overflow-hidden border border-grey-300 shadow-sm'>
-                        <img className='w-full h-full object-cover bg-white' src={profileData?.image || '/default-profile.jpg'} alt='user pofile'/>
-                    </div>
-                    <div className="flex flex-col items-center gap-2">
-                        <p className="text-bold text-lg">@{profileData?.username}</p>
-                        {profileData?.full_name && <p className="text-lightgrey">{profileData.full_name}</p>}
-                        <address className="text-lightgrey">{profileData?.email}</address>
-                    </div>
-                </div>
-
-                {/* soc links */}
-                <div className="flex flex-col w-full items-center gap-3 my-5">
-                    <h3>Social Links: </h3>
-                    {profileData?.twitter === null && profileData?.facebook === null && <p className="text-lightgrey italic text-center">Author Have No Connected Social Links!</p> }
-
-                    {/* //check if facebook or twitter exissts */}
-                    {profileData?.facebook 
-                    && 
-                    <a href={profileData.facebook}> <Facebook /></a>}
-                    
-                    {profileData?.twitter 
-                    && 
-                    <a href={profileData?.twitter}>
-                        <Twitter />
-                    </a>
-                    }
-
-                    <p>
-                        Joined: <span>{profileData?.date && formatDate(profileData?.date)}</span>
-                    </p>
-                    {/* // bio */}
-                    {profileData?.bio && 
-                        <div className="flex flex-col items-center gap-3 min-h-10 w-60 md:w-100 border-b-1 border-lightgrey rounded-md pb-5">
-                            <h2 className="text-white text-sm">bio</h2>
-                            <p className="text-sm text-lightgrey">
-                                "${profileData?.bio}"
-                            </p>
+        <>
+            {isLoading && <ProfilePageSkeleton />}
+            {isError && <div>Unexpected Error! refresh page or return to Home page </div>}
+            {data && (
+                <div className="px-4 flex flex-col">
+                    <main className="w-full flex flex-col md:flex-row md:justify-between">
+                        {/* user image, bio  */}
+                        <div className="w-full md:w-1/2 flex flex-col items-center gap-2">
+                            <div className='w-50 h-50 rounded-full overflow-hidden border border-grey-300 shadow-sm'>
+                                <img className='w-full h-full object-cover bg-white' src={data?.image || '/default-profile.jpg'} alt='user pofile'/>
+                            </div>
+                            <div className="flex flex-col items-center gap-2">
+                                <p className="text-bold text-lg">@{data?.username}</p>
+                                {data?.full_name && <p className="text-lightgrey">{data.full_name}</p>}
+                                <address className="text-lightgrey">{data?.email}</address>
+                            </div>
                         </div>
-                    }
-                </div>
-            </main>
 
-            {/* user posts */}
-            {profileData && <ProfilePosts username={profileData?.username}/>}
-        </div>
+                        {/* soc links */}
+                        <div className="flex flex-col w-full items-center gap-3 my-5">
+                            <h3>Social Links: </h3>
+                            {data?.twitter === null && data?.facebook === null && <p className="text-lightgrey italic text-center">Author Have No Connected Social Links!</p> }
+
+                            {/* //check if facebook or twitter exissts */}
+                            {data?.facebook 
+                            && 
+                            <a href={data.facebook}> <Facebook /></a>}
+                            
+                            {data?.twitter 
+                            && 
+                            <a href={data?.twitter}>
+                                <Twitter />
+                            </a>
+                            }
+
+                            <p>
+                                Joined: <span>{data?.date && formatDate(data?.date)}</span>
+                            </p>
+                            {/* // bio */}
+                            {data?.bio && 
+                                <div className="flex flex-col items-center gap-3 min-h-10 w-60 md:w-100 border-b-1 border-lightgrey rounded-md pb-5">
+                                    <h2 className="text-white text-sm">bio</h2>
+                                    <p className="text-sm text-lightgrey">
+                                        "${data?.bio}"
+                                    </p>
+                                </div>
+                            }
+                        </div>
+                    </main>
+
+                    {/* user posts */}
+                    {data && <ProfilePosts username={data?.username}/>}
+                </div>
+            )}
+        </>
     )
 }
--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -6,7 +6,6 @@ import type { RegisterFormDataType } from "../types/auth/RegisterFormData";
 
 import type { LoginResponseType } from "../types/auth/LoginResponse";
 import type { RegisterResponseType } from "../types/auth/RegisterResponse";
-import type { ProfileResponseType } from "../types/auth/ProfileResponse";
 
 export const loginUser = (data: LoginFormDataType): Promise<AxiosResponse<LoginResponseType>> => {
     return api.post('/users/token/', data)
@@ -14,8 +13,4 @@ export const loginUser = (data: LoginFormDataType): Promise<AxiosResponse<LoginR
 
 export const registerUser = (data: RegisterFormDataType): Promise<AxiosResponse<RegisterResponseType>> => {
     return api.post('/users/register/', data)
-}
-
-export const getUserData = (username: string): Promise<AxiosResponse<ProfileResponseType>> => {
-    return api.get(`/users/profile/${username}/`)
 }

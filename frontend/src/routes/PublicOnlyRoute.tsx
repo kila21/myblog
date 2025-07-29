fb953ed7b201b3f9cbe1b27d2df2eb6d78f82d5f
kila <luka.kilasonia.1@btu.edu.ge>
2025-07-29 19:29:44 +0400

    [routes] public only routes.

---
+++ frontend/src/routes/PublicOnlyRoute.tsx
@@ -0,0 +1,10 @@
+// src/routes/PublicOnlyRoute.tsx
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAppSelector } from '../store/hooks';
+
+const PublicOnlyRoute = () => {
+  const isAuthenticated = useAppSelector(state => state.auth.user);
+  return isAuthenticated ? <Navigate to="/" replace /> : <Outlet />;
+};
+
+export default PublicOnlyRoute;

beb29643da5d4f9f7164393ade59a47801637724
kila <luka.kilasonia.1@btu.edu.ge>
2025-08-03 19:08:21 +0400

    [category] display active item

--- frontend/src/components/category/CategoryList.tsx
+++ frontend/src/components/category/CategoryList.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import { useGetCategoriesQuery } from "../../store/posts/postsService"
 
 export const CategoryList = () => {
@@ -5,13 +7,18 @@ export const CategoryList = () => {
         refetchOnMountOrArgChange: true
     })
 
+    const [active, setActive ] = useState<null | number>(null)
+
     return (
         <div className="w-full flex flex-col items-center gap-3">
             <h2>Filter Posts By Category</h2>
             <div className="w-full flex flex-wrap gap-5 justify-center">
                 {categories?.results && categories.results.map(category => {
                     return (
-                        <div className="w-25 h-35 md:h-40 md:w-30 border-1 rounded-md flex flex-col items-center cursor-pointer" key={'id of' + category.title + category.id}>
+                        <div className={`w-25 h-35 md:h-40 md:w-30 rounded-md flex flex-col items-center cursor-pointer ${active === category.id ? 'border-2 border-blue-500' : 'border-1'}`}
+                            key={'id of' + category.title + category.id}
+                            onClick={() => setActive(category.id)}
+                        >
                             <img src={category.image} alt={category.title + 'posts'} className="w-full h-2/3"/>
                             <h5 className='text-sm'>{category.title}</h5>
                             <p className="text-md text-lightgrey">posts: <span className="text-white">{category.post_count}</span> </p>

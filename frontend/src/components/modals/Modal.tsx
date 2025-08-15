560447f976c75afb0ad42427b9401b4f10f76e7b
kila <luka.kilasonia.1@btu.edu.ge>
2025-08-15 18:16:16 +0400

    [update] modal min height

--- frontend/src/components/modals/Modal.tsx
+++ frontend/src/components/modals/Modal.tsx
@@ -5,7 +5,7 @@ export const Modal = ({ isOpen, onClose, title, children }: ModalProps) => {
 
   return (
     <div className="fixed inset-0 flex justify-center items-center z-50">
-      <div className="bg-[#23262F] rounded-lg p-5 md:w-96 w-70 h-80 relative">
+      <div className="bg-[#23262F] rounded-lg p-5 md:w-96 w-70 min-h-80 relative">
         {title && <h2 className="text-lg font-bold mb-3 text-white">{title}</h2>}
         <button
           onClick={onClose}
